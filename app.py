c3fcf64a0171f671d5ae16d02e352b09a2e6292c
Lukas <21Nuga@pm.me>
2025-05-03 17:59:43 +0200

    chore: initialize Flask app with SQLAlchemy and SQLite
    - Added app.py with basic Flask setup
    - Configured SQLAlchemy using SQLite (library.sqlite)

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+from data_models import db, Author, Book
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+
+db.init_app(app)
